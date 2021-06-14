9d89ec5aed7f4f33d21e722e92224683707600ff
Jan Radziminski <radziminski.j@gmail.com>
2021-06-14 17:04:57 +0200

    Refactored project

--- source/main.py
+++ source/main.py
@@ -4,8 +4,10 @@
 
 from test import test_custom_md5_hash_function
 
-INPUT_FILES = ['test-message-1.txt',
-               'test-message-2.txt', 'test-message-3.txt',  'test-message-4.txt']
+DATA_FOLDER = 'data'
+
+INPUT_FILES = [f'{DATA_FOLDER}/test-message-1.txt',
+               f'{DATA_FOLDER}/test-message-2.txt', f'{DATA_FOLDER}/test-message-3.txt',  f'{DATA_FOLDER}/test-message-4.txt']
 
 print('=======================================================')
 print('Md5 Hash function implementation made by Jan Radzimiński')
